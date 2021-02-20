7bc69d6367d170e8f002cd4c7897c33ad55da56f
ova <ovechkin.va@gmail.com>
2021-02-21 00:48:39 +0300

    added toast message for buttons

--- app/src/main/java/com/ovaprog/moviesearch/MainActivity.kt
+++ app/src/main/java/com/ovaprog/moviesearch/MainActivity.kt
@@ -2,10 +2,39 @@ package com.ovaprog.moviesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initMenuButtons()
     }
+
+    private fun initMenuButtons() {
+        button_menu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        button_favorites.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+
+        button_watch_later.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+
+        button_collections.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+
+        button_settings.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
 }
